aaf407244225f3b4e929ef8c342359a13ebadb82
Vishal Brahmbhatt <brahmbhattvishal4016@gmail.com>
2022-08-30 20:32:18 +0530

    reverse_level_order

---
+++ DSA_practise/src/Binary_Tree/reverse_level_order.java
@@ -0,0 +1,46 @@
+package Binary_Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import Binary_Tree.invertTree.TreeNode;
+
+public class reverse_level_order {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+	public static void reverseLevel(TreeNode root) {
+		if(root==null)
+			return ;
+		Stack<TreeNode> stk= new Stack<>();
+		Queue<TreeNode> q= new LinkedList<reverse_level_order.TreeNode>();
+		q.add(root);
+		while(!q.isEmpty()) {
+			root = q.peek();
+			q.remove();
+			q.add(root.right);
+			q.add(root.left);
+			stk.push(root);
+		}
+		while(!stk.isEmpty()) {
+			System.out.print(root.val);
+		}
+	}
+}
