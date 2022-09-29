8edd6c292fbe22e52b83086bfa2746e9ea49ef82
Vishal Brahmbhatt <brahmbhattvishal4016@gmail.com>
2022-09-29 11:50:43 +0530

    k_th_largest

--- DSA_practise/src/Striver/LinkedList/reverse_linkedlist.java
+++ DSA_practise/src/Striver/LinkedList/reverse_linkedlist.java
@@ -1,17 +1,73 @@
 package Striver.LinkedList;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class reverse_linkedlist {
 
-	// iterative
-	static ListNode reverse(ListNode head) {
-		ListNode newHead = null;
-		while (head != null) {
-			ListNode next = head.next;
-			head.next = newHead;
-			newHead = head;
-			head = next;
+	public static void main(String[] args) {
+		int a[] = new int[] { 1, 2, 3, 4, 5, 6 };
+		ListNode root = new ListNode(1);
+		root.next = new ListNode(2);
+		root.next.next = new ListNode(3);
+		root.next.next.next = new ListNode(4);
+		root.next.next.next.next = new ListNode(5);
+
+		LinkedList<Integer> ls = new LinkedList<Integer>();
+		ls = reverse(root);
+		for (Integer i : ls) {
+			System.out.print(i);
 		}
-		return newHead;
+		System.out.println();
+
+	}
+
+	// iterative
+	static LinkedList<Integer> reverse(ListNode head) {
+		LinkedList<Integer> ls1 = new LinkedList<Integer>();
+		ls1 = Node_2_List(head);
+		Collections.reverse(ls1);
+		ListNode test = List_2_Node(ls1);
+		ls1 =Node_2_List(test);
+//		ListNode newHead = null;
+//		while (head != null) {
+//			ListNode next = head.next;
+//			head.next = newHead;
+//			newHead = head;
+//			head = next;
+//		}
+		return ls1;
+	}
+
+	static LinkedList<Integer> Node_2_List(ListNode a) {
+		LinkedList<Integer> ls = new LinkedList<Integer>();
+		if (a == null) {
+			return ls;
+		}
+		Stack<ListNode> s = new Stack<ListNode>();
+		s.add(a);
+
+		while (!s.isEmpty()) {
+			a = s.pop();
+			ls.add(a.val);
+			if (a.next != null)
+				s.push(a.next);
+		}
+		return ls;
+	}
+	
+	static ListNode List_2_Node(LinkedList<Integer> ls) {
+		ListNode test = new ListNode();
+		Stack<Integer> s = new Stack<>();
+
+	
+		while(!ls.isEmpty()) {
+			s.add(ls.pop());
+			test.val = s.pop();
+		}
+		
+		return null;
 	}
 
 	// recursive
---
+++ DSA_practise/src/Striver/String/kth_largest_heap.java
@@ -0,0 +1,55 @@
+package Striver.String;
+
+import java.util.Arrays;
+import java.util.Random;
+
+//Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+//Output: 4
+public class kth_largest_heap {
+	public static void main(String[] args) {
+		int a[] = new int[] { 5, 3, 2, 9, 4, 0 };
+		int k = 2;
+		System.out.println(findKthLargest(a, k));
+		System.out.println(findkthLarget_quickselect(a, k));
+
+	}
+
+	public static int findKthLargest(int[] n, int k) {
+		Arrays.sort(n);
+		return n[n.length - k];
+	}
+
+	public static int findkthLarget_quickselect(int[] nums, int k) {
+		return quickSelect(nums, 0, nums.length - 1, k);
+	}
+
+	private static int quickSelect(int[] nums, int left, int right, int k) {
+		int pivot = left;
+		// use quick sort's idea
+		// put nums that are <= pivot to the left
+		// put nums that are > pivot to the right
+		for (int j = left; j < right; j++) {
+			if (nums[j] <= nums[right]) {
+				swap(nums, pivot++, j);
+			}
+		}
+		swap(nums, pivot, right);
+
+		// count the nums that are > pivot from high
+		int count = right - pivot + 1;
+		// pivot is the one!
+		if (count == k)
+			return nums[pivot];
+		// pivot is too small, so it must be on the right
+		if (count > k)
+			return quickSelect(nums, pivot + 1, right, k);
+		// pivot is too big, so it must be on the left
+		return quickSelect(nums, left, pivot - 1, k - count);
+	}
+
+	private static void swap(int[] nums, int x, int y) {
+		int temp = nums[x];
+		nums[x] = nums[y];
+		nums[y] = temp;
+	}
+}
